631385e9c68e5dd97ab5e06733d27244c50d559e
nobuhiko.kudo <nobuhiko.kudo@rakus.co.jp>
2015-02-16 15:45:05 +0900

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,6 +31,7 @@ RSpec.configure do |config|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
+  
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
@@ -88,4 +89,9 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+  ENV["RAILS_ENV"]||='test'
+  require File.expand_path("../../config/environment",__FILE__)
+  require 'rspec/rails'
+  config.include Capybara::DSL
 end
