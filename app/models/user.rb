a898616f558716e1ae2c46278c131c88f06b5f6c
nobuhiko.kudo <nobuhiko.kudo@rakus.co.jp>
2015-02-24 14:56:35 +0900

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ActiveRecord::Base
 
-	before_save { self.email = email.downcase }
+	#before_saveの書き換え
+	#before_save { self.email = email.downcase }
+	before_save { email.downcase! }
 	
 	has_secure_password
 
 	validates :name, presence: true, length: { maximum:50 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+#	正規表現の書き換え
+#	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	validates :email, presence: true, 
 					  format: { with: VALID_EMAIL_REGEX}, 
 					  uniqueness: { case_sensitive: false }
